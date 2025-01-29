40c5c99130d25ca3b731e1abf5ad64ca01eded8d
RonaldCBC <ronald@cbccreative.com>
2025-01-29 11:27:04 -0600

    Partially complete Product Configurator Template

---
+++ js/product-configurator.js
@@ -0,0 +1,9 @@
+const prodConfigData = {
+  heroDesktopUrl: '',
+  heroMobileUrl: '',
+  bannerTitle: 'Volo Configurator',
+  pageTitle: 'Select a Volo Typical',
+  pageDesc: 'Select an image below to customize in 3D and see pricing.',
+};
+
+$(window).on('load', function () {});
