c7da8105002be10f2b54965731bee17928381449
Ronald Fuquay <matsu_oukan@hotmail.com>
2025-05-08 06:45:29 -0500

    Commit to get files up on Github for URLS

---
+++ js/glightbox.js
@@ -0,0 +1,39 @@
+const lightbox = GLightbox({
+    selector: '.glightbox'
+});
+
+const lightboxVideo = GLightbox({
+    selector: '.glightboxVideo'
+});
+
+lightboxVideo.on('slide_changed', ({ prev, current }) => {
+    console.log('Prev slide', prev);
+    console.log('Current slide', current);
+
+    const { slideIndex, slideNode, slideConfig, player } = current;
+
+    if (player) {
+        if (!player.ready) {
+            // If player is not ready
+            player.on('ready', (event) => {
+                // Do something when video is ready
+            });
+        }
+
+        player.on('play', (event) => {
+            console.log('Started play');
+        });
+
+        player.on('volumechange', (event) => {
+            console.log('Volume change');
+        });
+
+        player.on('ended', (event) => {
+            console.log('Video ended');
+        });
+    }
+});
+
+const glightboxIframe = GLightbox({
+    selector: '.glightboxIframe'
+});
